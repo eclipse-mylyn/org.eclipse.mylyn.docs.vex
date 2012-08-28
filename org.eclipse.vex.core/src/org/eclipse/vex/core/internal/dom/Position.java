9742b5cb89cac354892d8b58f79c64c3e0155fbc
Florian Thienel <florian@thienel.org>
2012-08-29 00:21:49 +0200

    Removed Positions get flagged "invalid" for a better ease of use.
    Signed-off-by: Florian Thienel <florian@thienel.org>

--- org.eclipse.vex.core/src/org/eclipse/vex/core/internal/dom/Position.java
+++ org.eclipse.vex.core/src/org/eclipse/vex/core/internal/dom/Position.java
@@ -13,6 +13,9 @@ package org.eclipse.vex.core.internal.dom;
 /**
  * Represents a logical location in a document. As the document is modified, existing <code>Position</code> objects are
  * updated to reflect the appropriate character offset in the document.
+ * 
+ * Positions can be invalid if they were removed from their associated Content instance. Invalid positions do not get
+ * updated on content modifications. They must not be used for anything anymore.
  */
 public interface Position {
 
@@ -20,11 +23,20 @@ public interface Position {
 		public int getOffset() {
 			return -1;
 		}
+
+		public boolean isValid() {
+			return false;
+		};
 	};
 
 	/**
 	 * @return the character offset corresponding to the position.
 	 */
 	int getOffset();
+
+	/**
+	 * @return true if this position is still valid and actively maintained by its creator
+	 */
+	boolean isValid();
 
 }
