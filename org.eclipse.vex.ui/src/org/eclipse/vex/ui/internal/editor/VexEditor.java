eclipse-mylyn/org.eclipse.mylyn.docs.vex

commit a9fea11aaffabf69d1a3d7d7c060889a70bef64c

fix content loading from JFace document
It was pure luck that the previous implementation worked, as the content
was assigned to the systemId filed of the InputSource.

Change-Id: I2bce4d90a6450b63fb0a51dfcb828206cd0bbb36
Signed-off-by: Carsten Hiesserich <carsten.hie@gmail.com>

org.eclipse.vex.ui/src/org/eclipse/vex/ui/internal/editor/VexEditor.java
@@ -732,8 +732,10 @@ public class VexEditor extends EditorPart {
 				}
 			}
 
+			// A InputStream is used here to avoid a in memeory copy of the documents content
+			final InputSource is = new InputSource(new DocumentInputStream(jFaceDoc));
+			// Set the systemId of the InputSource to resolve relative URIs
 			final IEditorInput input = getEditorInput();
-			final InputSource is = new InputSource(jFaceDoc.get());
 			if (input instanceof IFileEditorInput) {
 				final IFile file = ((IFileEditorInput) input).getFile();
 				is.setSystemId(file.getLocationURI().toString());

org.eclipse.vex.ui/src/org/eclipse/vex/ui/internal/editor/DocumentInputStream.java
@@ -1,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Carsten Hiesserich and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Carsten Hiesserich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.vex.ui.internal.editor;
+
+import java.io.InputStream;
+
+import org.eclipse.jface.text.BadLocationException;
+
+/**
+ * This class wraps a {@link org.eclipse.jface.text.IDocument JFace Document} in an InputStream.
+ */
+public class DocumentInputStream extends InputStream {
+
+	private final org.eclipse.jface.text.IDocument document;
+	private int position;
+	private final int length;
+
+	public DocumentInputStream(final org.eclipse.jface.text.IDocument document) {
+		this.document = document;
+		length = document.getLength();
+		position = 0;
+	}
+
+	/**
+	 * Reads the next byte of data from this input stream. The value byte is returned as an int in the range 0 to 255.
+	 * If no byte is available because the end of the stream has been reached, the value -1 is returned.
+	 * 
+	 * @returns the next byte of data, or -1 if the end of the stream has been reached.
+	 */
+
+	@Override
+	public synchronized int read() {
+		try {
+			return position < length ? document.get(position++, 1).getBytes()[0] & 0xff : -1;
+		} catch (final BadLocationException e) {
+			return -1;
+		}
+	}
+
+}

commit fd3f5207d96a54f2dc166ce450cef8309808e6fb

fix charset issues when reading a document
The DocumentInputStream introduced with commit
a9fea11aaffabf69d1a3d7d7c060889a70bef64c did not handle UTF-8.

Change-Id: I25bf404bd003e4aea517f1a0c7abfb8f929fe3ec
Signed-off-by: Carsten Hiesserich <carsten.hie@gmail.com>

org.eclipse.vex.ui/src/org/eclipse/vex/ui/internal/editor/VexEditor.java
@@ -16,6 +16,7 @@
 package org.eclipse.vex.ui.internal.editor;
 
 import java.io.File;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
@@ -74,6 +75,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.dialogs.SaveAsDialog;
+import org.eclipse.ui.editors.text.IStorageDocumentProvider;
 import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.part.FileEditorInput;
@@ -732,8 +734,17 @@ public class VexEditor extends EditorPart {
 				}
 			}
 
-			// A InputStream is used here to avoid a in memeory copy of the documents content
-			final InputSource is = new InputSource(new DocumentInputStream(jFaceDoc));
+			String encoding;
+			if (provider instanceof IStorageDocumentProvider) {
+				// Try to get the encoding from the DocumentProvider
+				encoding = ((IStorageDocumentProvider) provider).getEncoding(getEditorInput());
+				System.out.println("encoding:" + encoding);
+			} else {
+				encoding = "UTF-8";
+			}
+			// A InputStream is used here to avoid an in memory copy of the documents content
+			final InputSource is = new InputSource(new InputStreamReader(new DocumentInputStream(jFaceDoc), encoding));
+
 			// Set the systemId of the InputSource to resolve relative URIs
 			final IEditorInput input = getEditorInput();
 			if (input instanceof IFileEditorInput) {