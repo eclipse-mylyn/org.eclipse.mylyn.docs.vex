e2a1c40ccb5fb9ca34add3233af2557ad4c3253f
Florian Thienel <florian@thienel.org>
2015-01-03 21:51:59 +0100

    provide traversal class for the parent axis
    Signed-off-by: Florian Thienel <florian@thienel.org>

---
+++ org.eclipse.vex.core/src/org/eclipse/vex/core/internal/boxes/ParentTraversal.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Florian Thienel and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * 		Florian Thienel - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.vex.core.internal.boxes;
+
+/**
+ * @author Florian Thienel
+ */
+public class ParentTraversal<T> extends BaseBoxVisitorWithResult<T> {
+
+	public ParentTraversal() {
+		super();
+	}
+
+	public ParentTraversal(final T defaultValue) {
+		super(defaultValue);
+	}
+
+	@Override
+	public T visit(final VerticalBlock box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final Frame box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final NodeReference box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final HorizontalBar box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final Paragraph box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final StaticText box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final TextContent box) {
+		return box.getParent().accept(this);
+	}
+
+	@Override
+	public T visit(final Square box) {
+		return box.getParent().accept(this);
+	}
+}
