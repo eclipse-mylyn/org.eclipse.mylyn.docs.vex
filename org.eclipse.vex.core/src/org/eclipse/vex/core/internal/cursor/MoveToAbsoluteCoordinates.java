c019a5429ea8ce44dd753a801da500deb7d6ebf9
Florian Thienel <florian@thienel.org>
2015-10-28 20:31:25 +0100

    remove (almost) use of ContentMap in MoveToAbsoluteCoordinates
    Signed-off-by: Florian Thienel <florian@thienel.org>

--- org.eclipse.vex.core/src/org/eclipse/vex/core/internal/cursor/MoveToAbsoluteCoordinates.java
+++ org.eclipse.vex.core/src/org/eclipse/vex/core/internal/cursor/MoveToAbsoluteCoordinates.java
@@ -10,14 +10,16 @@
  *******************************************************************************/
 package org.eclipse.vex.core.internal.cursor;
 
-import org.eclipse.vex.core.internal.boxes.BaseBoxVisitorWithResult;
+import java.util.LinkedList;
+
+import org.eclipse.vex.core.internal.boxes.DepthFirstTraversal;
+import org.eclipse.vex.core.internal.boxes.IBox;
 import org.eclipse.vex.core.internal.boxes.IContentBox;
-import org.eclipse.vex.core.internal.boxes.StructuralNodeReference;
 import org.eclipse.vex.core.internal.boxes.ParentTraversal;
+import org.eclipse.vex.core.internal.boxes.StructuralNodeReference;
 import org.eclipse.vex.core.internal.boxes.TextContent;
 import org.eclipse.vex.core.internal.core.Graphics;
 import org.eclipse.vex.core.internal.core.Rectangle;
-import org.eclipse.vex.core.internal.cursor.ContentMap.Environment;
 
 /**
  * @author Florian Thienel
@@ -44,7 +46,8 @@ public class MoveToAbsoluteCoordinates implements ICursorMove {
 
 	@Override
 	public int calculateNewOffset(final Graphics graphics, final ContentMap contentMap, final int currentOffset, final IContentBox currentBox, final Rectangle hotArea, final int preferredX) {
-		final IContentBox box = findClosestBoxOnLineByCoordinates(contentMap, x, y);
+		final IContentBox outmostContentBox = contentMap.getOutmostContentBox();
+		final IContentBox box = findClosestBoxByCoordinates(outmostContentBox, x, y);
 		if (box.containsCoordinates(x, y)) {
 			return box.getOffsetForCoordinates(graphics, x - box.getAbsoluteLeft(), y - box.getAbsoluteTop());
 		} else if (box.isLeftOf(x)) {
@@ -60,41 +63,86 @@ public class MoveToAbsoluteCoordinates implements ICursorMove {
 		}
 	}
 
-	private static IContentBox findClosestBoxOnLineByCoordinates(final ContentMap contentMap, final int x, final int y) {
-		final Environment environment = contentMap.findEnvironmentForCoordinates(x, y, false);
-		if (environment.deepestContainer == null) {
-			return contentMap.getOutmostContentBox();
+	private static IContentBox findClosestBoxByCoordinates(final IContentBox outmostContentBox, final int x, final int y) {
+		final IContentBox deepestContainer = findDeepestContainerOfCoordinates(outmostContentBox, x, y);
+		if (deepestContainer == null) {
+			return outmostContentBox;
 		}
-		return environment.deepestContainer.accept(new BaseBoxVisitorWithResult<IContentBox>() {
+		return findClosestBoxInContainer(deepestContainer, x, y);
+	}
+
+	private static IContentBox findDeepestContainerOfCoordinates(final IContentBox rootBox, final int x, final int y) {
+		return rootBox.accept(new DepthFirstTraversal<IContentBox>() {
 			@Override
 			public IContentBox visit(final StructuralNodeReference box) {
-				final IContentBox closestOnLine = environment.neighbours.getClosestOnLine().box;
-				if (closestOnLine != null) {
-					return closestOnLine;
+				if (!box.containsCoordinates(x, y)) {
+					return null;
+				}
+				final IContentBox deeperContainer = super.visit(box);
+				if (deeperContainer != null) {
+					return deeperContainer;
 				}
 				return box;
 			}
 
 			@Override
 			public IContentBox visit(final TextContent box) {
+				if (!box.containsCoordinates(x, y)) {
+					return null;
+				}
 				return box;
 			}
 		});
 	}
 
+	private static IContentBox findClosestBoxInContainer(final IContentBox container, final int x, final int y) {
+		final LinkedList<IContentBox> candidates = new LinkedList<IContentBox>();
+		container.accept(new DepthFirstTraversal<Object>() {
+			@Override
+			public Object visit(final TextContent box) {
+				if (box.containsY(y)) {
+					candidates.add(box);
+				}
+				return null;
+			}
+		});
+
+		int minHorizontalDistance = Integer.MAX_VALUE;
+		IContentBox closestBox = container;
+		for (final IContentBox candidate : candidates) {
+			final int horizontalDistance = horizontalDistance(candidate, x);
+			if (horizontalDistance < minHorizontalDistance) {
+				minHorizontalDistance = horizontalDistance;
+				closestBox = candidate;
+			}
+		}
+
+		return closestBox;
+	}
+
+	private static int horizontalDistance(final IBox box, final int x) {
+		if (box.getAbsoluteLeft() > x) {
+			return box.getAbsoluteLeft() - x;
+		}
+		if (box.getAbsoluteLeft() + box.getWidth() < x) {
+			return x - box.getAbsoluteLeft() - box.getWidth();
+		}
+		return 0;
+	}
+
 	private static boolean isLastEnclosedBox(final IContentBox enclosedBox) {
-		final IContentBox parent = findParentContentBox(enclosedBox);
+		final IContentBox parent = getParentContentBox(enclosedBox);
 		if (parent == null) {
 			return true;
 		}
 		return enclosedBox.getEndOffset() == parent.getEndOffset() - 1;
 	}
 
-	private static IContentBox findParentContentBox(final IContentBox child) {
-		return child.accept(new ParentTraversal<IContentBox>() {
+	private static IContentBox getParentContentBox(final IContentBox childBox) {
+		return childBox.accept(new ParentTraversal<IContentBox>() {
 			@Override
 			public IContentBox visit(final StructuralNodeReference box) {
-				if (child == box) {
+				if (box == childBox) {
 					return super.visit(box);
 				}
 				return box;
