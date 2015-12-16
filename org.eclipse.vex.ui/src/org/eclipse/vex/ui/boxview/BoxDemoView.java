0117da0aa7d021d447ce71e7f1aaa22058a39ab3
Florian Thienel <florian@thienel.org>
2015-12-16 19:58:39 +0100

    add support for loading a CSS file from the workspace
    Signed-off-by: Florian Thienel <florian@thienel.org>

--- org.eclipse.vex.ui/src/org/eclipse/vex/ui/boxview/BoxDemoView.java
+++ org.eclipse.vex.ui/src/org/eclipse/vex/ui/boxview/BoxDemoView.java
@@ -11,9 +11,19 @@
 package org.eclipse.vex.ui.boxview;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
@@ -26,6 +36,8 @@ import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IViewSite;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.vex.core.internal.css.StyleSheet;
 import org.eclipse.vex.core.internal.css.StyleSheetReader;
@@ -45,6 +57,7 @@ import org.w3c.css.sac.InputSource;
 public class BoxDemoView extends ViewPart {
 
 	private static final int SAMPLE_COUNT = 25;
+	private static final IPath CSS_WORKSPACE_FILE = new Path("/test/box-demo.css");
 
 	private Composite boxWidgetParent;
 	private BoxWidget boxWidget;
@@ -58,6 +71,32 @@ public class BoxDemoView extends ViewPart {
 			offsetLabel.setText(caretPositionAsText(selection));
 		}
 	};
+
+	private final IResourceChangeListener resourceChangeListener = new IResourceChangeListener() {
+		@Override
+		public void resourceChanged(final IResourceChangeEvent event) {
+			try {
+				event.getDelta().accept(new IResourceDeltaVisitor() {
+					@Override
+					public boolean visit(final IResourceDelta delta) throws CoreException {
+						if (!delta.getFullPath().equals(CSS_WORKSPACE_FILE)) {
+							return true;
+						}
+						reloadStyleSheet();
+						return false;
+					}
+				});
+			} catch (final CoreException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+
+	@Override
+	public void init(final IViewSite site) throws PartInitException {
+		super.init(site);
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(resourceChangeListener, IResourceChangeEvent.POST_CHANGE);
+	}
 
 	@Override
 	public void createPartControl(final Composite parent) {
@@ -83,6 +122,7 @@ public class BoxDemoView extends ViewPart {
 	public void dispose() {
 		super.dispose();
 		boxWidget = null;
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(resourceChangeListener);
 	}
 
 	@Override
@@ -115,15 +155,30 @@ public class BoxDemoView extends ViewPart {
 		boxWidgetParent.setFocus();
 	}
 
+	private void reloadStyleSheet() {
+		boxWidget.setBoxModelBuilder(new CSSBasedBoxModelBuilder(readStyleSheet()));
+		rebuildBoxModel();
+	}
+
 	private StyleSheet readStyleSheet() {
 		try {
-			final InputSource inputSource = new InputSource(new InputStreamReader(getClass().getResourceAsStream("box-demo.css"), "utf-8"));
+			final InputStream styleSheetInputStream;
+			final IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(CSS_WORKSPACE_FILE);
+			if (file.exists()) {
+				styleSheetInputStream = file.getContents();
+			} else {
+				styleSheetInputStream = getClass().getResourceAsStream("box-demo.css");
+			}
+
+			final InputSource inputSource = new InputSource(new InputStreamReader(styleSheetInputStream, "utf-8"));
 			return new StyleSheetReader().read(inputSource, null);
 		} catch (final UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (final CSSException e) {
 			e.printStackTrace();
 		} catch (final IOException e) {
+			e.printStackTrace();
+		} catch (final CoreException e) {
 			e.printStackTrace();
 		}
 		return null;
